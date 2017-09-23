b6f8888ca5e2f72e5b77fcad7201f34ae5085fc6
arsen4ik98 <arsenbeglaryan98@gmail.com>
2017-09-23 11:56:33 +0300

    Сделал метод пусто и take top

--- Queue/Program.cs
+++ Queue/Program.cs
@@ -7,6 +7,19 @@ namespace Queue
     {
         public static void Main(string[] args)
         {
+            IQueue asd = new Queue();
+            asd.Push(17);
+            asd.Push(33);
+            asd.Push(1);
+            asd.Push(3);
+            asd.Push(2);
+            Console.WriteLine(asd.Pop());
+            Console.WriteLine(asd.TakeTop());
+            Console.WriteLine(asd.IsEmpty());
+            asd.Push(17);
+            Console.WriteLine(asd.Pop());
+            Console.ReadKey();
+
         }
     }
 }
---
+++ test/Class1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Queue;
+using NUnit.Framework;
+
+namespace test
+{
+    [TestFixture]
+    public class Class1
+    {
+
+        [Test()]
+       
+        public void Test1()
+        {
+            Queue asd = new Queue();
+           asd.Push(2);
+            int expected = 1;
+            int actual = asd.count;
+        }
+
+
+
+
+    }
+}
