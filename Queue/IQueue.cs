f08aeb481236aa40646621693527b96701ac8d72
wmelina <wmelina@localhost.localdomain>
2017-09-23 10:17:26 +0300

    inception

---
+++ Queue/IQueue.cs
@@ -0,0 +1,14 @@
+﻿using System.Globalization;
+
+namespace Queue
+{
+    public interface IQueue
+    {
+        void Push (int take);
+        int Pop();
+        bool IsEmpty();
+        int TakeTop();
+
+
+    }
+}
---
+++ Queue/Queue.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Globalization;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Queue
+{
+    public class Queue : IQueue
+    {
+        private int[]Que = new int[10];
+        private int count = 0;
+        
+        public int Pop()
+        {
+            if (Que.Length == 0)
+            {
+                throw new Exception("massive is empty");
+            }
+            else
+            {
+                
+            }
+            
+        } 
+
+        public void Push(int take)
+        {
+            if (Que.Length == count)
+            {
+                Array.Resize(ref Que, Que.Length * 2);
+            }
+            else
+            {
+                Que[count] = take;
+                count++;
+            }
+        }
+
+        bool IQueue.IsEmpty()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        int IQueue.TakeTop()
+        {
+            throw new System.NotImplementedException();
+        }
+        /*public T TakeTop()
+        {
+            
+        }*/
+    }
+}
